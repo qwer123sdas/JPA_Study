2b03e739790d9816744ecbe2246a7b8faeed0a20
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-08-05 21:25:30 +0900

    d오타 수정

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -42,7 +42,7 @@ public class Order {
     }
 
     public void addOrderItem(OrderItem orderItem){
-        orderItems.add(orderItem)
+        orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
