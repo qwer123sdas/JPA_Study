4f525683b34b8760e51a71063b546d83f83f1c19
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-08-08 16:18:52 +0900

    아이템 레파지토리 + 서비스

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager em;
+
+    public void save(Item item){
+        if(item.getId() == null){
+            // 신규 등록
+            em.persist(item);
+        }else{
+            // 이미 등록된 디비에 저장(update같은)
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id){
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll(){
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+
+
+}
